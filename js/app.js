2a9ff0c92401dd5e77dbe179f823556a0e83073a
TheBrightSparkDev <danharriesxd@gmail.com>
2021-12-11 08:51:00 +0000

    lesson activity

---
+++ js/app.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+let homePage = document.getElementById("home-page");
+console.log(homePage);
+let sections = homePage.getElementsByClassName("section");
+console.log(sections);
